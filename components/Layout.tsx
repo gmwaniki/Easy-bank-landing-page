0cf687ec3384a9b92d9afff53d8ce45ae341f7b0
Gnm <georgenmwaniki@gmail.com>
2022-07-08 13:07:35 +0300

    Added Footer to layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,13 @@
 import Navbar from './Navbar';
+import Footer from './Footer';
+import type { ReactElement } from 'react';
 
-const Layout: React.FC<{ children: JSX.Element }> = ({ children }) => {
+const Layout: React.FC<{ children: ReactElement }> = ({ children }) => {
   return (
     <>
       <Navbar />
       <main>{children}</main>
+      <Footer />
     </>
   );
 };
