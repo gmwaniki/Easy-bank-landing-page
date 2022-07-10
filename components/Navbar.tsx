da4247015483b8ebad7cc3a290dbe3018fb2fdf5
Gnm <georgenmwaniki@gmail.com>
2022-07-10 18:43:30 +0300

    set image styles using next/image

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,5 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import logo from '../images/logo.svg';
-import hamburgerIcon from '../images/icon-hamburger.svg';
-import closeIcon from '../images/icon-close.svg';
 import React, { useState } from 'react';
 
 const Navbar = () => {
@@ -11,16 +8,24 @@ const Navbar = () => {
     setShowMenu(!showMenu);
   };
   return (
-    <header className='bg-CustomWhite sticky top-0 z-10 py-6 sm:py-0  '>
-      <div className='container mx-auto px-4 grid grid-cols-2 gap-4 items-center  sm:grid-cols-[auto,1fr,auto]  '>
+    <header className='bg-CustomWhite sticky top-0 z-10 py-5 sm:py-0  '>
+      <div className='container mx-auto px-4 grid grid-cols-2 justify-between items-center justify-items-start  sm:grid-cols-[auto,1fr,auto]  '>
         <Link href='/'>
           <a className='flex items-center'>
-            <Image src={logo} alt='easybank logo' priority></Image>
+            <div className='relative h-5 w-36'>
+              <Image
+                src='/images/logo.svg'
+                alt='easybank logo'
+                priority
+                layout='fill'
+                objectFit='contain'
+              />
+            </div>
           </a>
         </Link>
         <nav
           id='navigation'
-          className={` ${
+          className={`${
             showMenu
               ? 'block absolute  top-[4.25rem] w-screen left-0 h-[100vh]  bg-gradient-to-b from-CustomDarkBlueTrans to-CustomWhiteTrans overscroll-contain sm:bg-none sm:static sm:h-auto sm:w-auto'
               : 'hidden'
@@ -28,7 +33,7 @@ const Navbar = () => {
         >
           <ul
             role='list'
-            className='bg-CustomWhite text-CustomGrayishBlue w-5/6 mx-auto text-center rounded relative top-5 py-8 grid gap-4 sm:grid-flow-col sm:gap-4 sm:static sm:py-0 sm:max-w-xl'
+            className='bg-CustomWhite text-CustomGrayishBlue w-11/12  mx-auto text-center rounded relative top-5 py-8 grid gap-4 sm:grid-flow-col md:gap-7 sm:static sm:py-0 sm:w-auto sm:max-w-xl'
           >
             <li className='sm:py-4 sm:relative sm:before:w-0 sm:before:transition-[width_color] sm:hover:text-black sm:hover:before:w-full sm:before:h-1 sm:before:bg-CustomLimeGreen sm:before:absolute sm:before:bottom-0 sm:before:left-0'>
               <Link href='/'>
@@ -60,14 +65,25 @@ const Navbar = () => {
         <button className='hidden sm:inline-block text-CustomWhite font-semibold bg-gradient py-2 px-4 rounded-full hover:opacity-80'>
           Request Invite
         </button>
-        <button
-          className='sm:hidden place-self-end flex justify-center items-center'
-          onClick={handleClick}
-        >
+        <button className='sm:hidden justify-self-end ' onClick={handleClick}>
           {showMenu ? (
-            <Image src={closeIcon} alt='close Icon' />
+            <div className='relative h-7 w-7'>
+              <Image
+                src='/images/icon-close.svg'
+                alt='close Icon'
+                layout='fill'
+                objectFit='contain'
+              />
+            </div>
           ) : (
-            <Image src={hamburgerIcon} alt='Menu Icon' />
+            <div className='relative h-7 w-7'>
+              <Image
+                src='/images/icon-hamburger.svg'
+                alt=''
+                layout='fill'
+                objectFit='contain'
+              />
+            </div>
           )}
         </button>
       </div>
